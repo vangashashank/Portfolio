8369bb121d50a50ea3f5b7c179f62c40d37a1447
vangashashank <vangashashank1@gmail.com>
2026-04-24 11:31:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import './index.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Achievements from './components/Achievements';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <div className="divider" />
+        <About />
+        <div className="divider" />
+        <Experience />
+        <div className="divider" />
+        <Projects />
+        <div className="divider" />
+        <Achievements />
+      </main>
+      <Footer />
+    </>
+  );
+}
